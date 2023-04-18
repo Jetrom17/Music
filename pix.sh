3700227435f8c22f15160f19c5491d2bf8a5c894
Jeiel <github.esvlc@slmail.me>
2023-04-18 11:47:33 -0300

    Update pix.sh

--- pix.sh
+++ pix.sh
@@ -4,5 +4,7 @@ echo "Copie chave aleatória: a93ea08c-4239-44d8-8ffa-6a5687b1ed1c" |lolcat
 sleep 2.0
 echo "--- Esta mensagem irá se autodestruir em 5 segundos ---" |lolcat
 sleep 5.0
+echo ""
 clear
+echo ""
 exit
